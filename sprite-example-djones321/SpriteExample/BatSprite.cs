06c1c5f9b809719515c480e38e4f4ebd6e657db9
djones321 <32394137+djones321@users.noreply.github.com>
2021-02-03 23:33:01 -0600

    Initial Upload
    cis580 sprite tutorial

---
+++ sprite-example-djones321/SpriteExample/BatSprite.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace SpriteExample
+{
+
+    public enum Direction
+    {
+        Down,
+        Right,
+        Up,
+        Left,
+    }
+
+    public class BatSprite
+    {
+        private Texture2D texture;
+
+        private double directionTimer;
+
+        private double animationTimer;
+        
+        private short animationFrame = 1;
+        
+        public Direction Direction;
+        
+        public Vector2 Position;
+
+        public void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("32x32-bat-sprite");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            directionTimer += gameTime.ElapsedGameTime.TotalSeconds;
+            if (directionTimer > 2.0)
+            {
+                switch (Direction)
+                {
+                    case Direction.Up:
+                        Direction = Direction.Down;
+                        break;
+                    case Direction.Down:
+                        Direction = Direction.Right;
+                        break;
+                    case Direction.Right:
+                        Direction = Direction.Left;
+                        break;
+                    case Direction.Left:
+                        Direction = Direction.Up;
+                        break;
+                }
+                directionTimer -= 2.0;
+            }
+            //move bat
+            switch (Direction)
+            {
+                case Direction.Up:
+                    Position += new Vector2(0, -1) * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;
+                case Direction.Down:
+                    Position += new Vector2(0, 1) * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;
+                case Direction.Right:
+                    Position += new Vector2(1, 0) * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;
+                case Direction.Left:
+                    Position += new Vector2(-1, 0) * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    break;
+            }
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            //update animation timer
+            animationTimer += gameTime.ElapsedGameTime.TotalSeconds;
+
+            //update animation frame
+            if(animationTimer > 0.3)
+            {
+                animationFrame++;
+                if(animationFrame > 3)
+                {
+                    animationFrame = 1;
+                }
+                animationTimer -= 0.3;
+            }
+
+            //draw sprite
+            var source = new Rectangle(animationFrame*32, (int)Direction * 32, 32, 32);
+            spriteBatch.Draw(texture, Position, source, Color.White);
+
+        }
+
+    }
+}
